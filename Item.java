1b25a2952e2364c5a9ba80f1c8984e6a98c6cdd3
ngc0202 <ngc0202@gmail.com>
2011-12-02 13:47:33 -0500

    Added limit flag.
    Signed-off-by: ngc0202 <ngc0202@gmail.com>

--- Item.java
+++ Item.java
@@ -18,6 +18,7 @@ public class Item {
     private String removeText;
     private boolean canSpawn;
     private boolean canMake;
+    private int limit;
     private String article;
     private String[][] actions;
     private String[][] outputs;
@@ -36,6 +37,7 @@ public class Item {
         outputs = findOutputs();
         canMake = findCanMake();
         makeReqs = findMakeReqs();
+        limit = findLimit();
     }
 
     public final String getName() {
@@ -80,6 +82,10 @@ public class Item {
 
     public final String[][] getMakeReqs() {
         return makeReqs;
+    }
+
+    public final int getLimit() {
+        return limit;
     }
 
     private File findFile() {
@@ -168,7 +174,7 @@ public class Item {
             }
             return check;
         } else {
-            return true;
+            return Main.world.getCount(this) < limit;
         }
     }
 
@@ -352,6 +358,22 @@ public class Item {
         return reqs;
     }
 
+    private int findLimit() {
+        try {
+            Scanner file = new Scanner(itemfile);
+            String[] acur;
+            String cur = "";
+            while(file.hasNext() && cur.equals("</flags>")){
+                cur = file.nextLine();
+                acur = cur.split(" ");
+                if(acur[0].equalsIgnoreCase("limit")){
+                    return Integer.parseInt(acur[1]);
+                }
+            }
+        } catch (FileNotFoundException ex) {}
+        return -1;
+    }
+
     public static String cleanWord(String word) {
         String cleanword = "";
         if (word != null) {
@@ -378,7 +400,8 @@ public class Item {
                 return true;
             }
         } catch (IOException ex) {
-        } catch (NullPointerException npe) {}
+        } catch (NullPointerException npe) {
+        }
         return false;
     }
 }
