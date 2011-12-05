7ee73aa27f9aa0db47d70d5719f61429e1bf6039
ffrogman <ffrogman@gmail.com>
2011-12-04 19:40:16 -0500

    Fixed bugs when world is full.
    Signed-off-by: ffrogman <ffrogman@gmail.com>
    
    Fixed wrong world size.
    
    Signed-off-by: ngc0202 <ngc0202@gmail.com>

--- abdjektReader.java
+++ abdjektReader.java
@@ -9,11 +9,13 @@ public class abdjektReader {
         if (verb.equals("spawn")) {
             if (world.canSpawn(subject.getURLName())) {
                 if (subject.canSpawn()) {
-                    world.add(subject);
-                    if (!Main.world.checkMax(1) && (subject.getSpawnText() == null || subject.getSpawnText().equals(""))) {
-                        System.out.println("You spawn " + subject.getArticle() + subject.getName() + ".");
-                    } else {
-                        System.out.println(subject.getSpawnText());
+                    if(world.add(subject)){
+                        if ((subject.getSpawnText() == null || subject.getSpawnText().equals(""))){            
+                            System.out.println("You spawn " + subject.getArticle() + subject.getName() + ".");
+                        } else {
+                            System.out.println("test");
+                            System.out.println(subject.getSpawnText());
+                        }
                     }
                 } else {
                     System.out.println("You are unable to spawn " + subject.getArticle() + subject.getName() + ".");
